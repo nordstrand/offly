53642a6cd1be2ef2a73920627eb678f0d61f8212
jonte <>
2014-06-09 11:10:26 +0200

    support for show

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-
-/*
-myProxy.on("interceptResponseContent", function (buffer, responseObject, isSsl, charset, callback) {
-  var content = buffer.toString('utf8');
-  var css = "<"+"link rel='stylesheet' href='http://axiak.github.com/filternet/blink.css'>";
-  callback(content.replace(/<\/head>/i, css + "</head>"));
-});
-*/
+var options = require("./lib/options");
 
 
-/*
-myProxy.on("interceptRequest", function (requestOptions, callback) {
-    console.log("REQ" + requestOptions.path);
-    callback(requestOptions);
-});
-*/
+options.parseArguments(process.argv) ;
 
-
-var request = require("request");
-
-(function() {
-} (process.argv))
-
-
-var argv = require('minimist')(process.argv.slice(2), { default : {
-                                                       file : "http.json",
-                                                       port : 8123}});
-
-if (argv._.indexOf("dump") != -1 ) {
-    require("./lib/dump").start(argv.file);
-} else if (argv._.indexOf("serve") != -1 ) {
-    require("./lib/serve").start(argv.file);
+if (options.contains("dump")) {
+    require("./lib/dump").start(options.get("file"));
+} else if (options.contains("serve")) {
+    require("./lib/serve").start(options.get("file"));
+} else if (options.contains("show")) {
+    require("./lib/show")(options.get("file"));
+} else if (options.get("version")) { 
+    console.log(require("./package.json").name + " version " + require("./package.json").version);
 } else {
     help();
 }
 
 
-
 function help() {
-    console.log(require("./package.json").name + " version " + require("./package.json").version);
-    console.log( "dump  - spin up intercepting proxy for dumping traffic to file"); 
-    console.log( "serve - serve file contents"); 
-
-    console.log( "--file=<path to spool file>");
-    
+    console.log(require("./package.json").name + " command [<options>]" );
+    console.log();
+    console.log("Commands");
+    console.log( "dump  - spin up proxy dumping intercepted traffic to file"); 
+    console.log( "serve - serve dump file contents over http");
+    console.log( "show  - show dump file contents");
+    console.log();
+    console.log("Options");
+    console.log( "--file=<path to dump file>");
+    console.log( "--port=<tcp port to listen to>");
+    console.log( "--debug");
+    console.log( "--version");
+ 
 }
